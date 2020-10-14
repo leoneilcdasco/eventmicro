1feb4e9a31962d7e8af1d24f0b813e5151a0a6fe
buildtechdev <buildtechdev@users.noreply.github.com>
2020-10-14 08:18:55 +0800

    Fix Date & Refactoring
    - Fixed next year events not showing
    - Created sp-event.js and move scripts in HTML in that file

--- pages/views.py
+++ pages/views.py
@@ -9,14 +9,18 @@ from django.contrib.auth.models import User
 from .models import *
 from .services import *
 
+# -----------------------------------------------------------------------------
+# Default page showing all available Webinars
+# -----------------------------------------------------------------------------
 def index(request):
     print('DEBUG>>> index(): rendering registration default page')
 
     today    = datetime.today()
     calendar = datetime(today.year, today.month, 1)
-    event_list = []
+    year     = today.year
 
-    for m in range(0, 12):     
+    event_list = []
+    for m in range(0, 24):     
         events = Event.objects.filter(event_date__year=calendar.year,
                                       event_date__month=calendar.month).order_by('event_date')
         if events:
@@ -28,11 +32,16 @@ def index(request):
             next_month = int(calendar.month + 1)
         else:
             next_month = 1
-        calendar = datetime(calendar.year, next_month, 1)
+            year += 1
+
+        calendar = datetime(year, next_month, 1)
 
     context = { 'event_list' : event_list }
     return render(request, 'index.html', context)
 
+# -----------------------------------------------------------------------------
+# Webinar form registration request handler
+# -----------------------------------------------------------------------------
 def register(request):
     print('DEBUG>>> register(): Registration API endpoint')
 
@@ -45,7 +54,7 @@ def register(request):
         phone      = request.POST['phone']
         dob        = request.POST['dob']
         gender     = request.POST['gender']
-        others      = request.POST['others']
+        others     = request.POST['others']
 
         event = Event.objects.filter(id=event_id).first()
         if event:
@@ -54,20 +63,33 @@ def register(request):
                                                                 phone=phone, dob=dob, gender=gender,
                                                                 others=others)
             #Send email invite
-            invite = { 'event_date' : event.event_date,
-                       'name'       : str(participant.first_name + ' ' + participant.last_name), 
-                       'invitation' : event.invitation,
-                       'tagline'    : event.tagline,
-                       'details'    : event.details,
-                       'photo'      : event.photo,
-                       'email'      : participant.email }
+            invite = {  'event_date'  : event.event_date,
+                        'name'       : str(participant.first_name + ' ' + participant.last_name), 
+                        'invitation' : event.invitation,
+                        'tagline'    : event.tagline,
+                        'details'    : event.details,
+                        'photo'      : event.photo,
+                        'email'      : participant.email }
+
             context['invite'] = invite
             send_registration_email(invite)
     
     return render(request, 'email_invite.html', context)
 
+# -----------------------------------------------------------------------------
+# Email template test endpoint only
+# -----------------------------------------------------------------------------
 def invite(request):
     print('DEBUG>>> invite(): rendering invite email HTML')
-    event = {}
-    #send_registration_email(event)
-    return render(request, 'email_invite.html')
+    
+    invite = {  'event_date' : datetime.today(),
+                'name'       : 'Sirhc Someroda', 
+                'invitation' : 'Zoom invite here https://zoom/meeting/1245',
+                'tagline'    : 'Test Tagline',
+                'details'    : 'The quick brown fox jump over the lazy dog near the bank of the river.',
+                'photo'      : 'web-defaults/placeholder.png',
+                'email'      : 'sirhcsomeroda@gmail.com' }
+
+    context = { 'invite' : invite }
+    send_registration_email(invite)
+    return render(request, 'email_invite.html', context)
